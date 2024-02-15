7700eb22c2aa9447355e36f151d859139fef1fb6
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-15 17:46:53 +0100

    added foreach to print information

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,26 @@
 
 <body>
 
-   
+   <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Trains</h2>
+            </div>
+            @foreach ($trains as $train)
+            <div class="col-3 my-5 ">
+                <h6>Azienda: {{$train->company_name}}</h6>
+                <h6>Stazione di partenza: {{$train->departure_station}}</h6>
+                <h6>Stazione di arrivo: {{$train->arrival_station}}</h6>
+                <h6>Orario di partenza: {{$train->departure_time}}</h6>
+                <h6>Orario di arrivo: {{$train->arrival_time}}</h6>
+                <h6>Codice Treno: {{$train->train_code}}</h6>
+                <h6>Numero Carrozze: {{$train->number_of_carriages}}</h6>
+                <h6>In orario: {{$train->in_time}}</h6>
+                <h6>Cancellato: {{$train->deleted}}</h6>
+            </div>
+            @endforeach
+        </div>
+   </div>
 
 </body>
 
