e212369b34a3e2366fd895689ee8eb2c1feff8fd
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-15 17:18:03 +0100

    added faker and controller

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Model\Train;
+use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainTableSeeder extends Seeder
 {
@@ -13,8 +14,22 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $newTrain = new Train();
+
+            $newTrain->company_name = $faker->words(1, true);
+            $newTrain->departure_station = $faker->words(2, true);
+            $newTrain->arrival_station = $faker->words(2, true);
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->numberBetween(1, 10000);
+            $newTrain->number_of_carriages = $faker->numberBetween(3, 15);
+            $newTrain->in_time = $faker->boolean();
+            $newTrain->deleted = $faker->boolean();
+
+            $newTrain->save();
+        };
     }
 }
