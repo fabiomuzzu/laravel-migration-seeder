f33e99e496cd29478e1c1a0f94165d0036970bc2
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-15 16:03:19 +0100

    starting table infos

--- database/migrations/2024_02_15_144829_create_trains_table.php
+++ database/migrations/2024_02_15_144829_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company_name');
+            $table->string('departure station');
+            $table->string('arrival_station');
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
             $table->timestamps();
         });
     }
