4233eee1506fbe4e54d51409ef403889d67cc3bb
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-15 17:30:35 +0100

    added faker seed

--- database/migrations/2024_02_15_144829_create_trains_table.php
+++ database/migrations/2024_02_15_144829_create_trains_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company_name', 255);
-            $table->string('departure station', 255);
+            $table->string('departure_station', 255);
             $table->string('arrival_station', 255);
-            $table->date('departure_time');
-            $table->date('arrival_time');
+            $table->time('departure_time');
+            $table->time('arrival_time');
             $table->string('train_code', 255)->unique();
             $table->tinyInteger('number_of_carriages');
             $table->boolean('in_time')->default(true);
